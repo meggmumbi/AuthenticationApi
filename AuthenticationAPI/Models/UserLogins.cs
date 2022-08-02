35375df30804dfa40f5469e63ce902a9bb024ec5
margaretmumbi16@gmail.com <margaretmumbi16@gmail.com>
2022-08-02 08:16:15 +0300

    Latest

---
+++ AuthenticationAPI/Models/UserLogins.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationAPI.Models
+{
+    public class UserLogins
+    {
+        [Required]
+        public string UserName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+        public UserLogins() { }
+
+    }
+}
